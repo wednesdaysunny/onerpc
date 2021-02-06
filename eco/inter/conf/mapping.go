5a4e6754154a6ed6a62bb5181a2bad836cf5b204
liuxiaochen <wednesdaysunny@gmail.com>
2021-02-06 23:18:07 +0800

    server

--- eco/inter/conf/mapping.go
+++ eco/inter/conf/mapping.go
@@ -3,13 +3,10 @@ package conf
 import "fmt"
 
 var (
-	ProdEnv        = "prod"
-	SitEnv         = "sit"
-	TestEnv        = "test"
-	RpcPort        = 10010
-	ServiceMapping = map[string]string{
-
-	}
+	ProdEnv = "prod"
+	SitEnv  = "sit"
+	TestEnv = "test"
+	RpcPort = 10010
 )
 
 func GenServiceAddr(env, svc string) string {
@@ -20,7 +17,7 @@ func GenServiceAddr(env, svc string) string {
 func GenServiceName(sn string) string {
 	env := ConfEnv()
 	if env == "test" || env == "prod" {
-		return ServiceMapping[sn]
+		return sn
 	}
 	return "127.0.0.1"
 }
@@ -34,7 +31,7 @@ func GenServicePort(svc string) int {
 	// debug in local, generate service port from 20000
 	// 参照前p47项目，通过byte转ascii值相加生成port，特殊情况下依然会存在冲突，可通过修改map中的value解决
 	port := 0
-	for _, bv := range []byte(svc + ServiceMapping[svc]) {
+	for _, bv := range []byte(svc + svc) {
 		port += int(bv)
 	}
 	return port + 20000
